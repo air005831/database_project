a905693de191f46fff17064d976e330d0496229e
air005831 <130114913+air005831@users.noreply.github.com>
2026-03-11 14:08:14 +0800

    Add files via upload

---
+++ sql/course.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- 主機: localhost
+-- 產生時間： 2026-03-11 06:06:50
+-- 伺服器版本: 5.7.17-log
+-- PHP 版本： 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `university2026`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` varchar(8) NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `dept_name` varchar(20) DEFAULT NULL,
+  `credits` decimal(2,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 資料表的匯出資料 `course`
+--
+
+INSERT INTO `course` (`course_id`, `title`, `dept_name`, `credits`) VALUES
+('BIO-101', 'Intro. to Biology', 'Biology', '4'),
+('BIO-301', 'Genetics', 'Biology', '4'),
+('BIO-399', 'Computational Biology', 'Biology', '3'),
+('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', '4'),
+('CS-190', 'Game Design', 'Comp. Sci.', '4'),
+('CS-315', 'Robotics', 'Comp. Sci.', '3'),
+('CS-319', 'Image Processing', 'Comp. Sci.', '3'),
+('CS-347', 'Database System Concepts', 'Comp. Sci.', '3'),
+('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', '3'),
+('FIN-201', 'Investment Banking', 'Finance', '3'),
+('HIS-351', 'World History', 'History', '3'),
+('MU-199', 'Music Video Production', 'Music', '3'),
+('PHY-101', 'Physical Principles', 'Physics', '4');
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
