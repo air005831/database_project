a5b16e50ce449fbf44a0e8caf0a4b7d2cf2de776
airPC <air005831@gmail.com>
2026-03-31 01:41:38 +0800

    移除create table

--- sql/faculty.sql
+++ sql/faculty.sql
@@ -1,23 +1,3 @@
-CREATE TABLE Faculty (
-    FacSSN         CHAR(11),
-    FacFirstName   VARCHAR(50)  NOT NULL,
-    FacLastName    VARCHAR(50)  NOT NULL,
-    FacCity        VARCHAR(50)  NOT NULL,
-    FacState       CHAR(2)      NOT NULL,
-    FacZipCode     CHAR(10)     NOT NULL,
-    FacHireDate    DATE,
-    FacDept        CHAR(6),
-    FacRank        CHAR(4),
-    FacSalary      DECIMAL(10,2),
-    FacSupervisor  CHAR(11),
-    CONSTRAINT PKFaculty PRIMARY KEY (FacSSN),
-    CONSTRAINT FKFacSupervisor FOREIGN KEY (FacSupervisor) 
-        REFERENCES Faculty(FacSSN)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
-
-
 INSERT INTO Faculty (FacSSN, FacFirstName, FacLastName, FacCity, FacState, FacDept, FacRank, FacSalary, FacSupervisor, FacHireDate, FacZipCode)
 VALUES 
 ('543-21-0987', 'VICTORIA', 'EMMANUEL', 'BOTHELL', 'WA', 'MS', 'PROF', 120000, NULL, '1996-04-15', '98011-2242'),
